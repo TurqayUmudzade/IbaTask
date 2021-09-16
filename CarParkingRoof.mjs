268e77124e9f535d98aad885f728dece1e26c795
Turqay <bmlk3644j@gmail.com>
2021-09-16 17:26:31 +0400

    Add comments

--- CarParkingRoof.mjs
+++ CarParkingRoof.mjs
@@ -4,19 +4,21 @@ I used the sliding window algorihtm with 2 pointers one on the left and one on t
 */
 const carParkingRoof = (cars, k) => {
 
-    if (k === 1) return 1
-    let arr = new Array(Math.max(...cars)).fill(0)
-    for (let i = 0; i < cars.length; i++) arr[cars[i] - 1] = 1
+    if (k === 1) return 1 //we only need one roof to cover one car
 
-    let min = Number.MAX_SAFE_INTEGER
-    let l = 0
+    let arr = new Array(Math.max(...cars)).fill(0)// initilize the array of size of the largest element in cars
+
+    for (let i = 0; i < cars.length; i++) arr[cars[i] - 1] = 1// fill in the spots
+
+    let min = Number.MAX_SAFE_INTEGER// biggest number since we need  to find the min
+    let left = 0
     let counter = 0
-    for (let r = 0; r < arr.length; r++) {
-        if (arr[r] === 1) counter++
-        while (counter === k) {
-            min = Math.min(r - l + 1, min)
-            if (arr[l] === 1) break
-            l++
+    for (let right = 0; right < arr.length; right++) {
+        if (arr[right] === 1) counter++//move left pointer until the desired window size is reached
+        while (counter === k && left < right) {
+            min = Math.min(right - left + 1, min)//calculate min
+            if (arr[left] === 1) counter-- //adjust counter if we remove any cars
+            left++//move left pointer
         }
     }
     return min
--- Tests.js
+++ Tests.js
@@ -2,14 +2,11 @@ import carParkingRoof from "./CarParkingRoof.mjs";
 import findCountries from "./findCoutries.mjs";
 
 let cars = [6, 2, 12, 7]
-// let cars = [6, 2, 12, 7]
-// let cars = [1, 6, 15]
 let k = 3
 
-console.log(carParkingRoof(cars, 1));
 console.log(carParkingRoof(cars, 2));
-console.log(carParkingRoof(cars, 3));
 console.log(carParkingRoof(cars, 4));
+console.log(carParkingRoof(cars, k));
 
 let board = [
     [5, 4, 4],
@@ -20,4 +17,6 @@ let board = [
     [1, 4, 4],
     [4, 1, 1]
 ]
+
+
 console.log(findCountries(board))
